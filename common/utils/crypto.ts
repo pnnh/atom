420cc5b370c2a9d5a858756760911e68761c4645
larry <xspanni@gmail.com>
2025-02-12 16:52:16 +0800

    增加一个以uuid返回md5值的方法

--- common/utils/crypto.ts
+++ common/utils/crypto.ts
@@ -8,3 +8,9 @@ export function encodeSHA256(data: string) {
 export function encodeMD5(data: string) {
     return md5(data).toString();
 }
+
+export function encodeMD5Format(data: string) {
+    const value = md5(data).toString();
+    // 32位小写，UUID格式
+    return `${value.substr(0, 8)}-${value.substr(8, 4)}-${value.substr(12, 4)}-${value.substr(16, 4)}-${value.substr(20, 12)}`;
+}
