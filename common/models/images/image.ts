74194d1983248676a6620e7d4f7f73f7e5553e0e
larry <xspanni@gmail.com>
2025-03-26 13:50:22 +0800

    调整图片模型

--- common/models/images/image.ts
+++ common/models/images/image.ts
@@ -1,11 +1,12 @@
-export interface NPPictureModel {
-    file: string
-    folder: string
+export interface MTPictureModel {
     status: number
     title: string
     create_time: string
     update_time: string
     description: string
     owner: string
-    urn: string
+    uid: string
+    keywords: string
+    file_path: string
+    ext_name: string
 }
