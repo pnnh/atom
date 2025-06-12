827da5789c2caf7c8a7cb087b88c88e684da26ac
larry <xspanni@gmail.com>
2025-06-12 19:00:31 +0800

    增加一个语言辅助方法

---
+++ common/language.ts
@@ -0,0 +1,50 @@
+export const langEn = 'en'
+export const langZh = 'zh'
+export const defaultLanguage = langEn
+export const supportedLanguages = [langEn, langZh]
+
+export function isSupportedLanguage(lang: string): boolean {
+    return supportedLanguages.includes(lang)
+}
+
+export function isLanguageEn(lang: string): boolean {
+    if (!lang) {
+        return false
+    }
+    return lang === langEn || lang.startsWith(`${langEn}-`)
+}
+
+export function isLanguageZh(lang: string): boolean {
+    if (!lang) {
+        return false
+    }
+    return lang === langZh || lang.startsWith(`${langZh}-`)
+}
+
+export function getLanguageFromPathname(pathname: string): string {
+    const segments = pathname.split('/')
+    if (segments.length > 1 && isSupportedLanguage(segments[1])) {
+        return segments[1]
+    }
+    return defaultLanguage
+}
+
+export function replaceLanguageInPathname(pathname: string, lang: string): string {
+    if (!isSupportedLanguage(lang)) {
+        lang = defaultLanguage
+    }
+    if (pathname === '/') {
+        if (lang === defaultLanguage) {
+            return '/'
+        } else {
+            return `/${lang}/`
+        }
+    }
+    const segments = pathname.split('/')
+    if (segments.length > 1 && isSupportedLanguage(segments[1])) {
+        segments[1] = lang
+    } else {
+        segments.splice(1, 0, lang)
+    }
+    return segments.join('/')
+}
