db2462b35881239218ee3eba0418741774bfd1c1
pnnh <linyangz@outlook.com>
2025-09-18 21:46:26 +0800

    将获取客户端配置函数由异步改为同步以方便浏览器端调用

--- client/config/config.ts
+++ client/config/config.ts
@@ -1,15 +1,8 @@
 'use client'
 
-import {IBrowserConfig} from "@/services/common/config";
 import {decodeBase58String} from "@/atom/common/utils/basex";
-import {usePublicConfig, useServerConfig} from "@/services/server/config";
 
-export async function useClientConfig() {
-    // run on server side
-    if (typeof window === 'undefined') {
-        const serverConfig = await useServerConfig()
-        return usePublicConfig(serverConfig)
-    }
+export function useClientConfig() {
     const lgEnv = document.getElementById('LGEnv') as HTMLInputElement
     if (!lgEnv) {
         throw Error('没有找到 LGEnv 元素，无法获取配置')
@@ -18,6 +11,5 @@ export async function useClientConfig() {
         throw Error('LGEnv 元素没有内容，无法获取配置')
     }
     const configText = decodeBase58String(lgEnv.value)
-    const clientConfig: IBrowserConfig = JSON.parse(configText)
-    return clientConfig
+    return JSON.parse(configText)
 }
